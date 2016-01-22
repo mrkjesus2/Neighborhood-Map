31b3fc8bbc2e0cc407ef5fb2155d54e80a89941f
Mark <markweig@gmail.com>
2016-01-22 13:55:43 -0500

    Add infoWindow to markers

--- app/scripts/map.js
+++ app/scripts/map.js
@@ -1,11 +1,8 @@
 app.Map = app.Map || {};
 
 app.Map = {
-  map: {},
-  placesApi: {},
-  // places: ko.observableArray(),
-
   init: function() {
+    this.infoWindow = new google.maps.InfoWindow;
     var home = {lat: 39.927677, lng: -75.171909};
     var el = document.getElementById('map-container');
     this.map = new google.maps.Map(el, {
@@ -14,25 +11,24 @@ app.Map = {
       minZoom: 12,
       maxZoom: 18
     });
-    this.status = google.maps.places.PlacesServiceStatus;
-
     console.log('Map Initialized'); // REMOVE
+    // Load places once the maps bounds are set
     google.maps.event.addListenerOnce(this.map, 'bounds_changed', this.getPlaces);
   },
 
   // Get a list of places from Google Maps and fill ViewModel places array
   getPlaces: function() {
     console.log('Getting Places'); // REMOVE
-    this.placesApi = new google.maps.places.PlacesService(app.Map.map);
 
+    var placesApi = new google.maps.places.PlacesService(app.Map.map);
     var request = {
       bounds: app.Map.map.getBounds(),
       types: ['restaurant']
     };
-    this.placesApi.nearbySearch(request, function(results, status) {
+
+    placesApi.nearbySearch(request, function(results, status) {
       if (status === 'OK') {
         results.forEach(function(result, idx) {
-          // app.Map.places.push(results[idx]);
           app.ViewModel.places.push(results[idx]);
           app.ViewModel.placesArr.push(results[idx]); //REMOVE
           app.Map.createMarker(result);
@@ -45,10 +41,11 @@ app.Map = {
 
   getPlaceDetails: function(place) {
     console.log('Getting Place Details'); // REMOVE
+    var placesApi = new google.maps.places.PlacesService(app.Map.map);
     var request = {
       placeId: place.place_id
     };
-    this.placesApi.getDetails(request, function(loc, status) {
+    placesApi.getDetails(request, function(loc, status) {
       if (status === 'OK') {
         return loc;
       } else {
@@ -63,17 +60,21 @@ app.Map = {
       attribution: {source: 'mrkjesus2.github.io/Neighborhood-Map'},
       icon: place.icon,
       map: app.Map.map,
-      //optimized: false,  // If problems with animation, uncomment
+      // optimized: false,  // If problems with animation, uncomment
       place: {
         location: place.geometry.location,
         placeId: place.place_id
       },
       title: place.name
     });
+    // TODO: Set the content for infoWindow
+    var info = place.name;
 
     // Add click event to the Marker
     google.maps.event.addListener(marker, 'click', function() {
-      // TODO: Add infoWindow, preferably with 3rd Party API calls
+      console.log(place.name, 'Marker Clicked');
+      app.Map.infoWindow.setContent(info);
+      app.Map.infoWindow.open(this.map, marker);
     });
 
   }
--- app/scripts/viewModel.js
+++ app/scripts/viewModel.js
@@ -6,4 +6,4 @@ app.ViewModel = {
 };
 
 ko.applyBindings(app.ViewModel);
-ko.applyBindings(app.Map);
+
