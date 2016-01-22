14aad9fdb2ab0a6aa6fcd3c587bff15a474d87fd
Mark <markweig@gmail.com>
2016-01-22 15:32:20 -0500

    Add jquery, foursquare.js

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -40,7 +40,7 @@ const reload = browserSync.reload;
 
 // Lint JavaScript
 gulp.task('lint', () =>
-  gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/knockout-3.4.0.js', '!app/scripts/maps.js'])
+  gulp.src(['app/scripts/**/*.js', '!app/scripts/vendor/knockout-3.4.0.js', '!app/scripts/vendor/jquery-2.2.0.js', '!app/scripts/map.js'])
     .pipe($.eslint())
     .pipe($.eslint.format())
     .pipe($.if(!browserSync.active, $.eslint.failOnError()))
