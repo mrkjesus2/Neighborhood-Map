cc772178d27ec0ec80fcfb41ed0f1e77b06e7313
Mark <markweig@gmail.com>
2016-01-27 20:51:48 -0500

    Saved

--- app/scripts/viewModel.js
+++ app/scripts/viewModel.js
@@ -2,7 +2,23 @@ app.ViewModel = app.ViewModel || {};
 
 app.ViewModel = {
   places: ko.observableArray(),
-  markers: ko.observableArray()
+  markers: ko.observableArray(),
+
+  listClick: function() {
+    // Select the marker for the place that was clicked
+  },
+
+  markerClick: function() {
+    // Highlight the name in the list for the marker that was clicked
+  },
+
+  listfilter: function() {
+    // Filter the list based on what is type in input box
+  },
+
+  markerfilter: function() {
+    // Remove markers that don't fit the input box text
+  }
 };
 
 ko.applyBindings(app.ViewModel);
